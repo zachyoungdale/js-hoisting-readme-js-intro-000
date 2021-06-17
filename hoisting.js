d384c7305124ae9e5a47ae932b5ad0ad089bdb89
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-17 20:51:00 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -21,8 +21,11 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
+  sayMy();
+
   function sayMy() {
     var name = "Kristin";
+
     console.log(name);
   }
   sayMy();
